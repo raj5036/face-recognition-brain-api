f89e4edf857909d40c4d0526417e382770e3c0ef
Raj <97rajmath@gmail.com>
2020-06-04 11:07:16 -0500

    finished building API

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express=require('express');
 const app=express();
 const bodyparser=require('body-parser');
+const bcrypt = require('bcrypt');
+const cors=require('cors');
 //body-parser middleware
 app.use(bodyparser.urlencoded( { extended:false } ));
 app.use(bodyparser.json());
+app.use(cors());
+
 const database={
     users:[
         {
@@ -25,9 +29,17 @@ const database={
     ],
 };
 app.get('/',(req,res)=>{
-    res.send("Server stared");
+    res.json(database.users);
 });
 app.post('/signin',(req,res)=>{
+    const hash="$2b$10$AsHVXfG/L8PR51/fSovL1.k6oO1mWK7A.jjHxynyp/sdivu.gF45e";
+    const myPlaintextPassword="12257tjhgbjhdtgwq";
+    bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
+        console.log(result);
+    });
+    bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
+        // result == false
+    });
     if(req.body.email===database.users[0].email && req.body.password===database.users[0].password){
         res.json(database.users[0]);
         return;
@@ -38,6 +50,9 @@ app.post('/register',(req,res)=>{
     const name=req.body.name;
     const email=req.body.email;
     const password=req.body.password;
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        console.log(hash);
+    });
 
     database.users.push({
             id:4,
